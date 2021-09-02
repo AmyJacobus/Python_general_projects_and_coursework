40ec706ee1fd7bb240059e2d5886c241e5261d7f
Amy <ammishaddaijacobus@gmail.com>
2021-09-02 14:47:20 -0500

    Cover_crop_calculator assignment (Fall 2021/Scripting Languages)

--- cover_crop_calculator.py
+++ cover_crop_calculator.py
@@ -1,25 +1,55 @@
-#Header
+# Header
+# Name: Ammishaddai Jacobus
+# Date: September 6, 2021
+# Description: This is a crop coverage calculator. It asks the user for the area lenght, the area width and it seeding
+# rate. The calculator takes that data, and inserts it into an algorithm and that calculates the crop coveraged needed
+# based on the data that the user inserted.
+
 # display a welcome message
-print("The crop cover calculator")
-print("It's pretty easy to use. Insert your data and the calculator will provide you with the data you need!")
+print('++' * 50)
+print(" ")
+print("The crop cover calculator version 1")
+print("Instructions: You will need the following data: (area Length, area width, and seeding rate) to use this calculator.")
+print(" ")
+print('++' * 50)
+print(" ")
+"""
+This code displays information about the application to the user. Like the name of the application, it's version and
+instructions on what information is needed to succesfully make use of this application.
+"""
 
 # get input from the user
-area_length = float(input(" Please type in the are length(.ft): \t"))
-area_width = float(input("Please type in the area width(.ft):\t"))
+area_length = float(input("Please type in the are length(.ft): "))
+area_width = float(input("Please type in the area width(.ft): "))
+"""
+These are two variables to take input from the user. Area length takes the user input for the length of the area, and 
+area width takes input about the area width and converts that data into float for both variables.
+
+"""
 
 # calculate miles per gallon
 acerage = (area_length * area_width) / 43560
-seeding_rate = float(input("Please type in the seeding rate (lbs):\t"))
-cover_crop_needed = float(acerage * seeding_rate)
+seeding_rate = float(input("Please type in the seeding rate (lbs): "))
+cover_crop_needed = (acerage * seeding_rate)
+"""
 
-print(cover_crop_needed)
+"""
 
-print(f"The data you inserted for length was: {area_length}.")
-print(f"The data you inserted for width was: {area_width}")
-print(f"The data you inserted for seeding rate was: {seeding_rate}.")
+print(" ")
+print(f"Your total needed crop coverage is: {cover_crop_needed} lbs.")
+"""
 
-print(f"The coverage you need is: {cover_crop_needed}.")
+
+"""
+
+print(" ")
+print('++' * 50)
+print(" ")
 print("Thank you for making use of our calculator!")
+print(" ")
+print("Designed by JCBSoftware & Design.BV")
+"""
 
-print("Designed by Amy Jacobus")
+
+"""
 
