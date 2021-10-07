527040e3f4e9b139feb25b63a8b29f47eda97272
Amy <ammishaddaijacobus@gmail.com>
2021-10-07 09:51:24 -0500

    Project finalized

---
+++ Midterm-Assignment/paradise_motel_booking.py
@@ -0,0 +1,23 @@
+# make sure to use random() for the - available rooms
+
+# Percentage increase should be:
+  # br *= 1.2 (day of week rate) instead of doing br = br +  (br*0.2)
+  # base rate could be a constant  like $80
+
+# Display when you display the chosen day
+# example MONDAY available Rooms .upper()
+# Make sure to search for atleast the first letter in the input, like double0er had un error, you can make sure it still
+# goes through by using some character capture thingy.
+
+# The program must have several validations
+
+# Original ideas:
+# - Add validation module for credit card or paypal payment system
+# - Based on your location - special rating br: (NE: $60, FL: $90, NY: $80), anywhere in  the U.S is $85
+# - International location - Base rate of $95
+
+# 3 Modules
+# Validation
+# Main
+# Credit card authorization
+# Semi - manual chatbot
