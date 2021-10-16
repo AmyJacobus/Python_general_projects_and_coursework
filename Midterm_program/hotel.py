a0c7e832f8df92000accab0736ab4f569fb1fa05
Amy <ammishaddaijacobus@gmail.com>
2021-10-16 10:42:07 -0500

    Progress, some bugs that prevent the program from running correctly, but overall the code is like 75% done.

--- Midterm_program/hotel.py
+++ Midterm_program/hotel.py
@@ -7,7 +7,7 @@ Description:
 """
 
 import random
-import validation
+import validation as v
 
 # authorship
 __author__ = 'Ammishaddai Jacobus'
@@ -15,7 +15,9 @@ __version__ = '2.0'
 __date__ = 'Oct 15, 2021'
 __status__ = 'Development'
 
-BASE_RATE = 80 # GLOBAL VARIABLE - CONSTANT
+
+BASE_RATE = 80  # GLOBAL VARIABLE - CONSTANT
+
 
 def welcome_msg():
 
@@ -28,60 +30,56 @@ def welcome_msg():
 
 def get_dow():
 
-        while True:
+    while True:
 
-            print("What day of the week would you like to check in? ")
-            dow = input('MONDAY - SUNDAY: ').upper()
-            if dow in ['Monday','TUESDAY','WEDNESDAY','THURSDAY','FRIDAY','SATURDAY']:
-                break
-            else:
-                print('Error! Please type in a day of the week.')
+        print("What day of the week would you like to check in? ")
+        dow = v.get_dow(prompt='MONDAY - SUNDAY')
 
         return dow
 
+
 def get_dow_rate(dow):
-        # calculate dow
-        if dow in ['SUNDAY', 'MONDAY', 'TUESDAY']:
-            return BASE_RATE * 1.2
-        elif dow == ['WEDNESDAY','THURSDAY']:
-            return (BASE_RATE * 1.1)
-        elif dow == ['FRIDAY','SATURDAY']:
-            return BASE_RATE
-        else:
-            print('Program error')
-            print('Exiting the program now')
-            exit()
+    # calculate dow
+    if dow in ['SUNDAY', 'MONDAY', 'TUESDAY']:
+        return BASE_RATE * 1.2
+    elif dow == ['WEDNESDAY','THURSDAY']:
+        return (BASE_RATE * 1.1)
+    elif dow == ['FRIDAY','SATURDAY']:
+        return BASE_RATE
+    else:
+        print('Program error')
+        print('Exiting the program now')
+        exit()
+
 
 def get_avail_rooms():
         # WHAT EXACTLY IS HAPPENING HERE
-        num_single = random.randint(0, 9)
-        num_double = random.randint(0, 11)
-        num_king= random.randint(0, 3)
+    num_single = random.randint(0, 9)
+    num_double = random.randint(0, 11)
+    num_king= random.randint(0, 3)
 
-        return num_single, num_double, num_king
+    return num_single, num_double, num_king
 
-# generate available rooms for single, double king using random numbers
 
-#
 def get_room_rates(dow_rate):
 
-        single_rate = dow_rate
-        double_rate = dow_rate * 15
-        king_rate = dow_rate * 1.25
+    single_rate = dow_rate
+    double_rate = dow_rate * 15
+    king_rate = dow_rate * 1.25
 
-        return single_rate, double_rate, king_rate
+    return single_rate, double_rate, king_rate
 
 
-def get_room_type_rate(prompt,dow, dow_rate):
+def get_room_type_rate(dow_rate):
 
      num_single, num_double, num_king = get_avail_rooms()
      single_rate, double_rate, king_rate = get_room_rates(dow_rate)
 
-     print(f'{dow} available rooms')
+     # print(f'{dow} available rooms')
      print('='*50)
-     print(f'{num_single}(2 guest max) available at {single_rate}')
-     print(f'{num_single}(4 guest max) available at {double_rate}')
-     print(f'{num_single}(2 guest max) available at {king_rate}')
+     print(f'{num_single} single rooms (2 guest max) available at {single_rate}')
+     print(f'{num_double} double rooms(4 guest max) available at {double_rate}')
+     print(f'{num_king} king rooms (2 guest max) available at {king_rate}')
 
      print('What room would you like to book: ')
 
@@ -96,18 +94,15 @@ def get_room_type_rate(prompt,dow, dow_rate):
          else:
              print('Invalid input, try again.')
 
+         return room_type
 
 def get_num_guests(room_type):
 
-        num_guests = input('How many guests will be staying in the room: ')
-        if num_guests == 'int':
-            return num_guests
-        else:
-            print('Invalid input')
-            exit()
+        num_guests = v.get_range(prompt='How many guests will be staying in the room: ', low=0, high=5)
+
+        return num_guests
 
 def calc_surcharge(num_guests):
-
 
         if num_guests == 1:
             surcharge = 0
@@ -121,38 +116,53 @@ def calc_surcharge(num_guests):
         return surcharge
 
 
-def many_days(room_type, num_guests):
+def get_number_of_nights(room_type, num_guests, room_type_rate):
 
     print(f'How many days will you want to book a {room_type} room,')
-    total_days = input(f'With {num_guests} at ')
+    num_nights = input(f'With {num_guests} at ')
+
+    return num_nights
 
 
 def display_booking(dow, dow_rate, room_type, room_type_rate, num_guests, surcharge):
-        # display
-        # a
-        # summary
-        # of
-        # the
-        # all
-        # booking
-        # information
-        # or end
-        # the
-        # program if invalid
-        # dow, and room_type is received
+    print()
+    print(f'You chose {dow}')
+    print(f'Room type: {room_type}')
+    print(f'With {num_guests}')
+    if num_guests > 0:
+        print(f'With a surcharge of ${surcharge}')
+        total_charge = room_type_rate + surcharge
+        print(f'So your total charge for this booking is: ${total_charge}')
+    else:
+        total_charge = room_type_rate
+        print(f'Your total charge for this booking is: ${total_charge}')
+
+
+def confirm_booking(num_nights, total_charge):
+        #  prompt the user to confirm their booking num_nights & total_charge
+        print(f'Would you like to confirm your booking for {num_nights} nights at the total charge of ${total_charge}')
+
+        while True:
+            confirm = v.get_choice(prompt='Y=Yes or N=no').lower()
+            if confirm in ['yes','y']:
+                print(f'Your total bill is ${total_charge}')
+                print('We are looking forward to seeing you soon!')
+                break
+            elif confirm in ['no','n']:
+                print('Thank you for making use of our service')
+                print('Hope to see you again soon')
+                exit()
 
 
 def get_do_you_want_to_continue():
         # ISSUE
         while True:
-
-         choice  = input('Would you like to book another room  [yes/no]: ').lower()
-         if choice == 'yes' or 'y':
-            return False
-         else:
-            return True
-
-# the user if they want to book another room (lower case input and validate: y, n, yes, no) return true or false
+          choice  = v.get_choice(prompt= 'Would you like to book another room  [yes/no]: ').lower()
+          if choice in ['yes','y']:
+              main()
+          elif choice in ['no','n']:
+              print('Thank you for making use of our booking service')
+              exit()
 
 
 def main():
--- Midterm_program/validation.py
+++ Midterm_program/validation.py
@@ -10,6 +10,18 @@ their error.
 
 # authorship
 
+def get_dow(prompt):
+
+    while True:
+        dow = input(f'{prompt}')
+        if dow in ['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY']:
+            return False
+        else:
+            print('Error! Please type in a day of the week.')
+            return True
+
+    return dow
+
 def get_num(prompt, data_type='int'):
 
     while True:
@@ -40,10 +52,40 @@ def get_pos(prompt, limit, data_type='int'):
         else:
             print(f'The number cannot be below {limit}')
 
+def get_range(prompt, low, high, data_type='int'):
+
+    while True:
+        user_input = input(f'{prompt} (Range = {low}-{high}): ')
+
+        if data_type == 'int':
+            number = int(user_input)
+        else:
+            number = float(user_input)
+
+        if number > low and number <= high:
+            return number
+
+        else:
+            print("ERROR")
+            exit()
+
+def get_choice(prompt):
+
+    while True:
+
+        choice = input(f'{prompt}').lower()
+        if choice in ['yes','y']:
+            break
+        elif choice in ['no','n']:
+            break
+        else:
+            print('You have not input a yes or no.Try again.')
+
+
 
 def main():
-    get_pos(prompt='How many rooms would you like to book ', limit=1)
-    get_num(prompt='input a number: ', data_type='float')
+    get_choice(prompt='yes or no: ')
+
 
 if __name__ == "__main__":
       main()
