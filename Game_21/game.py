a68834245ce4899c6351171c3e4f022085fa6f4e
Amy <ammishaddaijacobus@gmail.com>
2021-12-02 10:15:06 -0600

    Added display message and get player functions to the game module.

---
+++ Game_21/game.py
@@ -0,0 +1,38 @@
+
+
+
+LENGTH = 100
+
+
+def display_msg():
+    print('$' * LENGTH)
+    print()
+    print('                                       WELCOME TO GAME 21')  # HOW TO STRING FORMAT
+    print()
+    print('$' * LENGTH)
+    print()
+    print('The rules are simple!')
+    print('1. Each player is trying to get as close to 21 without going over.')
+    print('2. Each player is ONLY trying to beat the dealer\'s hand.')
+    print('3. The dealer will keep dealing himself cards until he beats all players hands or goes over 21.')
+    print('4. Tie goes to the player, not the dealer.')
+    print('5. Each player gets dealt two card between 1 - 10.')
+    print('6. The player then can choose to receive additional cards.')
+    print('7. Each player starts with $1.00.')
+    print('8. Default bet is 25 cents, but the player can double it after holding.')
+    print('9. Winner is the last person with cash, not including the dealer.')
+
+def get_players():
+    # Create a dictionary or pass an empty dictionary
+    # while loop
+    # Add funds etc to the dictionary
+
+
+
+
+def main():
+    display_msg()
+
+
+if __name__ == "__main__":  # Basically if the name of the module is equal to main
+    main()  # Run this specific program.
