5494acd5c61ecaedb4b3207b4c8d783031985ec5
Amy <ammishaddaijacobus@gmail.com>
2021-12-05 17:43:20 -0600

    Added function get_players and function play.

--- Game_21/game.py
+++ Game_21/game.py
@@ -1,15 +1,29 @@
+#!/usr/bin/env python3
+
+"""
+Programmers:
+Date:
+Description:
+"""
+
+# Authorship information # Done by Amy
+__author__ = 'Ammishaddai Jacobus and Ben Sadler'
+__version__ = '1.0'
+__date__ = 'NEED TO BE DONE'
+__status__ = 'Development'
 
 
+LINE_LENGTH = 100
 
-LENGTH = 100
+
 
 
 def display_msg():
-    print('$' * LENGTH)
+    print('$' * LINE_LENGTH)
     print()
     print('                                       WELCOME TO GAME 21')  # HOW TO STRING FORMAT
     print()
-    print('$' * LENGTH)
+    print('$' * LINE_LENGTH)
     print()
     print('The rules are simple!')
     print('1. Each player is trying to get as close to 21 without going over.')
@@ -22,16 +36,50 @@ def display_msg():
     print('8. Default bet is 25 cents, but the player can double it after holding.')
     print('9. Winner is the last person with cash, not including the dealer.')
 
-def get_players():
+
+def get_players(players):
     # Create a dictionary or pass an empty dictionary
     # while loop
     # Add funds etc to the dictionary
 
+    # How do we add to the dictionary with while loops? SINCE DEBBIES EXAMPLE IS LIKE A 2D/3D dictionary
+    print()
+    print('=' * LINE_LENGTH)
+    print('Now lets get this game setup.  Who is all playing?')
+    print('Please enter a player\'s name or enter \'done\' when finished.')
+    print()
+    while True:
+        name = input('Enter name: ') # NEEDS FORMATTING F-STRING
+        if name != 'Done':
+            continue
+        else:
+            break
 
+def play(players):
+    print()
+    print('=' * LINE_LENGTH)
+    print('Starting game...')
+    print('=' * LINE_LENGTH)
+    print()
+
+    # NEED TO DETECT PLAYER FROM THE DICTIONARY (KEY AND ACCESS THEIR INFORMATION)
+      # - RANDOM NUMBER FOR CARDS TO GENERATE 2 CARDS FOR PLAYER
+
+    # should use while or for loop here
+    input('Do you want another card? (y=yes, n=no): ')
+    print('Cards: RANDOM NR1, RANDOM NR2, RANDOM NR3')
+    # if user says yes, continue to add more cards, until they go over 21 (then they lose!)
+    # if they choose no, display player name, holds at  (Card total)
+
+    print('Do you want to double your 25 cent beat? (y=yes, n=no): ')
 
 
 def main():
-    display_msg()
+
+    players = {}  # We start with an empty dictionary
+
+    display_msg(players)
+    get_players(players)
 
 
 if __name__ == "__main__":  # Basically if the name of the module is equal to main
